6b52a5b036674502016c9f7a4a238a33987bb5c5
mrsamsonn <andrewsj.ahs@gmail.com>
2024-03-11 14:26:01 -0700

    import dependencies

--- app/script.py
+++ app/script.py
@@ -1,2 +1,16 @@
 def func():
     return "hello there"
+
+
+    #importing dependencies
+#gudie: https://medium.com/the-handbook-of-coding-in-finance/stock-prices-prediction-using-long-short-term-memory-lstm-model-in-python-734dd1ed6827
+
+import math
+import yfinance as yf
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler 
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
