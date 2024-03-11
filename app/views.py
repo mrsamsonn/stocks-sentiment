5baf90d5097193d1d1ccf1eacc48e673af66a21d
mrsamsonn <andrewsj.ahs@gmail.com>
2024-03-11 15:28:23 -0700

    ltsm can now show plot to html

--- app/views.py
+++ app/views.py
@@ -1,6 +1,44 @@
+import matplotlib.pyplot as plt
+import io
+import base64
+from PIL import Image
 from django.shortcuts import render
-from .script import func
+from .script import ltsm
+
+def compress_image(image):
+    # Open the image using PIL
+    img = Image.open(image)
+
+    # Convert the image to RGB mode if it's in RGBA mode
+    if img.mode == 'RGBA':
+        img = img.convert('RGB')
+
+    # Resize the image to reduce dimensions (adjust dimensions as needed)
+    img = img.resize((800, 600))
+
+    # Compress and save the image to a bytes buffer
+    buffer = io.BytesIO()
+    img.save(buffer, format='JPEG', quality=70)
+    buffer.seek(0)
+    
+    return buffer
+
 
 def index(request):
-    output = func
-    return render(request, 'index.html', {'output': output})
+    fig, ax = ltsm()
+
+    # Convert the Matplotlib figure to a PNG image in memory
+    buffer = io.BytesIO()
+    fig.savefig(buffer, format='png')
+    buffer.seek(0)
+
+    # Compress the image
+    compressed_buffer = compress_image(buffer)
+
+    # Encode the compressed image buffer to base64
+    plot_image = base64.b64encode(compressed_buffer.getvalue()).decode()
+    compressed_buffer.close()
+    buffer.close()
+
+    # Pass the base64 encoded image to the HTML template
+    return render(request, 'index.html', {'plot_image': plot_image})
--- stocks_sentiment/settings.py
+++ stocks_sentiment/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import matplotlib
+
+matplotlib.use('Agg')
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
