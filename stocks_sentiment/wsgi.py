1dfe1887ea371dc9cbf3bd51008f4aa2449ee9c3
mrsamsonn <andrewsj.ahs@gmail.com>
2024-03-11 13:44:31 -0700

    init django

--- stocks_sentiment/wsgi.py
+++ stocks_sentiment/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sentiment_analysis project.
+WSGI config for stocks_sentiment project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sentiment_analysis.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stocks_sentiment.settings')
 
 application = get_wsgi_application()
